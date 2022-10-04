1a268f9c76cd2971df593ac5a0d8075869fc6836
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2022-10-04 14:47:48 +0200

    bus API fetch

--- js/bus.js
+++ js/bus.js
@@ -1,21 +1,29 @@
 const url = "https://xmlopen.rejseplanen.dk/bin/rest.exe/multiDepartureBoard?id1=851400602&id2=851973402&rttime&format=json&useBus=1"
 let apiData;
 
-fetch(url)
-.then((response) => {
-    //parsing data
-    return response.json()
-})
-.then((data) => {
-    //The data you wanna use
-    console.log(data);
-    apiData = data
-})
-.catch((error) => {
-    //If theres an error
-    console.error(error)
-})
-.finally(() => {
-//When all is set and done
-//document.querySelector('#text').innerHTML = `<p>${apiData.MultiDepartureBoard.Departure[0].name}</p>`
-})
+for (let index = 0; index < 6; index++) {
+    fetch(url)
+        .then((response) => {
+            //parsing data
+            return response.json()
+        })
+        .then((data) => {
+            //The data you wanna use
+            console.log(data);
+            apiData = data.MultiDepartureBoard.Departure[index]
+
+        })
+        .catch((error) => {
+            //If theres an error
+            console.error(error)
+        })
+        .finally(() => {
+            //When all is set and done
+            document.querySelector('#bus').innerHTML += `<p>${apiData.line}</p>
+<p>${apiData.direction}</p>
+<p>${apiData.time}</p>
+`
+        })
+
+
+}
