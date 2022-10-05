a8c7e84ecec733c3b040bf9df994b24341314492
Philip Thomsen <philip-th@live.dk>
2022-10-05 08:59:45 +0200

    kantine js
    lavet fetch samt caching så api'et ikke bliver kaldt for tit

--- js/menu.js
+++ js/menu.js
@@ -2,55 +2,51 @@
     const STORAGE_KEY = "canteen"
     let apiData;
     const meal = document.querySelector('#meal')
-
-
+    let menu = JSON.parse(localStorage.getItem('canteen'))
+    console.log(menu);
+    
     if (today.getDay() == localStorage.getItem('today')) {
-    meal.innerText = localStorage.getItem(STORAGE_KEY).Days[0].Dish
+        switch (today.getDay()) {
+            case 1:
+                meal.innerText = menu.Days[0].Dish
+                break;
+                case 2:
+                    meal.innerText = menu.Days[1].Dish
+                    break;
+                    case 3:
+                meal.innerText = menu.Days[2].Dish
+                break;
+                case 4:
+                meal.innerText = menu.Days[3].Dish
+                break;
+                case 5:
+                meal.innerText = menu.Days[4].Dish
+                break;
+        
+            default:
+                break;
+        }
     } 
     else
     {
-        fetch(url)
-        .then(response => {
-            return response.json();
-            //parsing our data
-        })
-        .then(data => {
-            apiData = data  
-        })
-        .catch(error => {
-            console.log(error);
-            //On error
-        })
-        .finally(() => {
-        localStorage.setItem('today', today.getDay())
-        localStorage.setItem(STORAGE_KEY, data)   
-        apiData =  localStorage.getItem(STORAGE_KEY)
-        meal.innerText = apiData.Days[1].Dish
-            
-        })  
-    } 
+ fetch(url)
+    .then(response => {
+        return response.json();
+        //parsing our data
+    })
+    .then(data => {
+        apiData = data  
+    })
+    .catch(error => {
+        console.log(error);
+        //On error
+    })
+    .finally(() => {
+    localStorage.setItem('today', today.getDay())
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(apiData))
     
-
-        setTimeout(() => {
-           switch (today.getDay()) {
-                case 1:
-                    meal.innerText = apiData.
-                    break;
-                    case 2:
-                        meal.innerText = apiData.Days[1].Dish
-                        break;
-                        case 3:
-                    meal.innerText = apiData.Days[2].Dish
-                    break;
-                    case 4:
-                    meal.innerText = apiData.Days[3].Dish
-                    break;
-                    case 5:
-                    meal.innerText = apiData.Days[4].Dish
-                    break;
-            
-                default:
-                    break;
-            }
-        }, 500);
+    meal.innerText = apiData.Days[1].Dish
         
+    }) 
+    apiData = localStorage.getItem(STORAGE_KEY)
+    } 
